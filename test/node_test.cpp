39269e01227f131fc3d56c632ceb20bd4d2fa02c
Spencer Carlson <spencerdcarlson@gmail.com>
2014-03-09 08:24:02 -0500

    node_test updated

--- test/node_test.cpp
+++ test/node_test.cpp
@@ -6,6 +6,7 @@ int main(){
         class node n(1,0,DVECTOR);
         class node n1(2,8,DVECTOR);
         n.add_neighbor(n1);
+        std::cout << n.to_string();
         n.remove_neighbor(2);
         std::cout << n.to_string();
 }
